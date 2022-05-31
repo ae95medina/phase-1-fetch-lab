3699ff84092e647a07c5125e8ed5737667c3ff42
ae95medina <ae95medina@gmail.com>
2022-05-31 12:15:13 -0500

    commit tree ed9c229278b4928ae891ca9fc585c950829ee5ae

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json()).then(json =>{
+  renderBooks(json)
+  })
+  return fetch('https://anapioficeandfire.com/api/books')
   // To pass the tests, don't forget to return your fetch!
-  
 }
 
-function renderBooks(books) {
+function renderBooks(json) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  json.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
